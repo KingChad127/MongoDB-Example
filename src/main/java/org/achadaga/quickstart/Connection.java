e6816b4daf61aab86ef29e2f1a67e52fe75f83f0
KingChad127 <abhinavc@cs.utexas.edu>
2021-12-31 23:29:43 -0600

    connect to atlas db

---
+++ src/main/java/org/achadaga/quickstart/Connection.java
@@ -0,0 +1,18 @@
+package org.achadaga.quickstart;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://spuser:stockport@spapp.eai5e.mongodb.net/SPApp?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            databases.forEach(db -> System.out.println(db.toJson()));
+        }
+    }
+}
---
+++ src/main/java/org/achadaga/quickstart/HelloMongoDB.java
@@ -0,0 +1,7 @@
+package org.achadaga.quickstart;
+
+public class HelloMongoDB {
+    public static void main(String[] args) {
+        System.out.println("Hello MongoDB!");
+    }
+}
